1fe221f7f7bb358a8d4c1560574a3ebbeb829966
aanujkhurana <aanujkhurana@gmail.com>
2023-10-28 11:41:39 +1000

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <nav>
-    <RouterLink to="/tree_chart">Tree</RouterLink>
-  </nav>
-  <RouterView/>
+  <Chart :data="data"></Chart>
 </template>
+<script>
+import * as d3 from 'd3';
+import Chart from './Chart';
+
+export default {
+  el: '#App',
+  data() {
+    return {
+      data: null,
+    };
+  },
+  components: {
+    Chart,
+  },
+  created() {
+    d3.csv(
+      'https://raw.githubusercontent.com/bumbeishvili/sample-data/main/org.csv'
+    ).then((d) => {
+      console.log('fetched data', d);
+      this.data = d;
+    });
+  },
+};
+</script>
