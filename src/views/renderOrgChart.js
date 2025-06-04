04602aa587db69d0bad42f90175cc1d7e550fad2
aanujkhurana <aanujkhurana@gmail.com>
2025-06-04 21:04:23 +1000

    update chart settings

--- src/views/renderOrgChart.js
+++ src/views/renderOrgChart.js
@@ -28,33 +28,14 @@ export function useOrgChart() {
     const chart = new OrgChart()
       .container(container)
       .data(data)
-      .scaleExtent([0.1, 1])
+      .scaleExtent([0.1, 0.75])
       .setActiveNodeCentered(true)
-      .nodeHeight(() => 380)
-      .nodeWidth(() => 376)
+      .nodeHeight(() => 230)
+      .nodeWidth(() => 380)
       .childrenMargin(() => 120)
-      .compactMarginBetween(() => 70)
+      .compactMarginBetween(() => 80)
       .compactMarginPair(() => 120)
       .siblingsMargin(() => 120)
-
-      .nodeUpdate(function (d) {
-        d3.select(this)
-          .select(".node-rect")
-          .attr("width", 386)
-          .attr("height", 385)
-          .attr("stroke", (d) =>
-            d.data._highlighted || d.data._upToTheRootHighlighted
-              ? "#4285F4"
-              : "none"
-          )
-          .attr("stroke-width", (d) =>
-            d.data._highlighted || d.data._upToTheRootHighlighted ? 8 : 2
-          )
-          .attr("y", -3)
-          .attr("x", -3)
-          .attr("stroke-linejoin", "round")
-          .style("stroke-alignment", "outer");
-      })
 
       .linkUpdate(function (d) {
         d3.select(this)
@@ -67,32 +48,33 @@ export function useOrgChart() {
         if (d.data._upToTheRootHighlighted) d3.select(this).raise();
       })
 
-      .onNodeClick((d) => {
-        clickedNodeID.value = d.id;
-        chart.setExpanded(d.id, !chart.isExpanded(d.id));
-        chart.render();
-      })
+      // .onNodeClick((d) => {
+      //   clickedNodeID.value = d.id;
+      //   chart.setExpanded(d.id, !chart.isExpanded(d.id));
+      //   chart.fit();
+      //   chartInstance.value?.render();
+      // })
 
-      .nodeButtonX(() => -150)
-      .nodeButtonY(() => -30)
+      .nodeButtonX(() => -80)
+      .nodeButtonY(() => -20)
 
       .buttonContent(({ node }) => {
         const foreignObject = d3.selectAll(".node-button-foreign-object");
-        foreignObject.attr("width", 300).attr("height", 50);
+        foreignObject.attr("width", 200).attr("height", 42);
 
         const text = node.children
-          ? `▲ Collapse ${node.data._directSubordinates} Parts`
-          : `▼ Expand ${node.data._totalSubordinates} Parts`;
+          ? `▲ Collapse ${node.data._directSubordinates} Nodes`
+          : `▼ Expand ${node.data._totalSubordinates} Nodes`;
 
         const gradient = node.children
           ? "linear-gradient(180deg, rgba(66,133,244,1) 0%, rgba(32,38,171,1) 100%)"
           : "linear-gradient(180deg, rgba(32,38,171,1) 0%, rgba(66,133,244,1) 100%)";
 
         return `
-          <div class="rounded-[42px] w-[360px] h-[52px] p-4 text-white text-center relative"
+          // <div class="rounded-[32px] w-[140px] h-[42px] p-3 text-white text-center relative"
                style="background: ${gradient};
                       box-shadow: 0px 4px 16px 0px rgba(0, 55, 146, 0.4);
-                      font: 600 15px sans-serif">
+                      font: 600 14px">
             ${text}
           </div>
         `;
@@ -113,20 +95,15 @@ export function useOrgChart() {
   };
 
   const fitChart = () => chartInstance.value?.fit();
-  const compactChart = () => {
-    if (!chartInstance.value) return;
-
-    chartInstance.value
-      .compact(true) // this is a setter in D3 Org Chart
-      .render()
-      .fit();
-  };
   const normalChart = () => {
-    chartInstance.value?.compact(false).render().fit();
+    chartInstance.value.compact(true).layout("top").render().fit();
+  };
+  const compactChart = () => {
+    chartInstance.value?.layout("left").render().fit();
   };
 
-  const expandAllNodes = () => chartInstance.value?.expandAll();
-  const collapseAllNodes = () => chartInstance.value?.collapseAll();
+  const expandAllNodes = () => chartInstance.value?.expandAll().fit();
+  const collapseAllNodes = () => chartInstance.value?.collapseAll().fit();
   const findRoot = (nodeID) => {
     if (!nodeID || !chartInstance.value) return;
 
